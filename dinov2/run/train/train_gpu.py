9c043cf96f4d7aaaf42b5a6c1e4bc0bc71d3ef9c
etienneguevel <etienneguev@gmail.com>
2024-06-03 15:25:51 +0200

    new train gpu

--- dinov2/run/train/train_gpu.py
+++ dinov2/run/train/train_gpu.py
@@ -5,21 +5,26 @@ import sys
 import torch
 import torch.multiprocessing as mp
 from dinov2.logging import setup_logging
-from dinov2.train.train import main as train_main
+from dinov2.train import main as train_main, get_args_parser as get_train_args_parser
 from dinov2.run.submit import get_args_parser
 
 
 logger = logging.getLogger("dinov2")
 
+def wrapper(rank, args):
+    train_main(*[args])
 
 def main():
     setup_logging()
 
-    args = get_args_parser.parse_args()
+    description = "Local launcher for DINOV2 training"
+    train_args_parser = get_train_args_parser(add_help=False)
+    args_parser = get_args_parser(description=description, parents=[train_args_parser])
+    args = args_parser.parse_args()
     assert os.path.exists(args.config_file), "Configuration file does not exist!"
 
     WORLD_SIZE = torch.cuda.device_count()
-    mp.spawn(train_main, nprocs=WORLD_SIZE)
+    mp.spawn(wrapper, args=[args], nprocs=WORLD_SIZE)
     return 0
 
 
