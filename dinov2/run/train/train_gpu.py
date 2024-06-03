1b1cd55d56c181b500de753040851a6ea7dda298
etienneguevel <etienneguev@gmail.com>
2024-06-03 15:25:51 +0200

    new train gpu

--- dinov2/run/train/train_gpu.py
+++ dinov2/run/train/train_gpu.py
@@ -3,23 +3,51 @@ import os
 import sys
 
 import torch
+import torch.distributed as dist
 import torch.multiprocessing as mp
 from dinov2.logging import setup_logging
-from dinov2.train.train import main as train_main
+from dinov2.train import get_args_parser as get_train_args_parser, SSLMetaArch
 from dinov2.run.submit import get_args_parser
-
+from dinov2.utils.config import get_cfg_from_args
 
 logger = logging.getLogger("dinov2")
+
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "12355"
+
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    dist.barrier()
+
+
+class Trainer:
+    def __init__(self, rank, args, world_size):
+        self.rank = rank
+        self.args = args
+        self.world_size = world_size
+
+    def __call__(self):
+
+        setup(self.rank, self.world_size)
+        cfg = get_cfg_from_args(self.args)
+
+        model = SSLMetaArch(cfg).to(torch.device(self.rank))
+        model.prepare_for_distributed_training()
+        logger.info("Model:\n{}".format(model))
 
 
 def main():
     setup_logging()
 
-    args = get_args_parser.parse_args()
+    description = "Local launcher for DINOV2 training"
+    train_args_parser = get_train_args_parser(add_help=False)
+    args_parser = get_args_parser(description=description, parents=[train_args_parser])
+    args = args_parser.parse_args()
     assert os.path.exists(args.config_file), "Configuration file does not exist!"
 
     WORLD_SIZE = torch.cuda.device_count()
-    mp.spawn(train_main, nprocs=WORLD_SIZE)
+    mp.spawn(Trainer, args=[args], nprocs=WORLD_SIZE)
     return 0
 
 
