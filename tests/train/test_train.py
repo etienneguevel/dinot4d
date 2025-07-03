15d6941c4ef85eb1d2600de3f79348cdff318eb9
etienneguevel <etienneguev@gmail.com>
2025-07-03 19:22:29 +0200

    fix: update tests

--- tests/train/test_train.py
+++ tests/train/test_train.py
@@ -1,33 +1,46 @@
+from argparse import Namespace
 import logging
 import pytest
 from pathlib import Path
 
 import torch
-from omegaconf import OmegaConf
-
 
 from dinov2.train.ssl_meta_arch import SSLMetaArch
 from dinov2.train.train import do_train
+from dinov2.utils.config import setup
+
 
 logger = logging.getLogger("dinov2")
 
 
 def test_one_epoch():
     if torch.cuda.is_available():
-        cfg = OmegaConf.load(Path(__file__).parent / "config.yaml")
+        config_path = Path(__file__).parent / "config.yaml"
+        output_path = Path(__file__).parent / "logs"
+
+        # Make an args object
+        args = Namespace(
+            output_dir=output_path,
+            config_file=config_path,
+            opts=[],
+        )
+
+        # Setup
+        cfg = setup(args)
 
         # Setup the correct paths for data loading and output
         dataset_path = Path(__file__).parent.parent / "data" / "dataset1"
-        output_path = Path(__file__).parent / "logs"
 
         cfg.train.dataset_path = dataset_path
         cfg.train.output_dir = output_path
 
         # Setup the model
         model = SSLMetaArch(cfg).to(torch.device("cuda"))
+        model.prepare_for_distributed_training()
 
         logger.info("Model:\n {}".format(model))
         do_train(cfg, model, resume=False)
+
     else:
         warning_msg = "No CUDA devices found, skipping test,"
         logger.warning(warning_msg)
